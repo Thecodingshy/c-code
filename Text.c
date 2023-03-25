c3f07524e24a401445dd13f28ffa65c5b2c009a5
Thecodingshy <127670043+Thecodingshy@users.noreply.github.com>
2023-03-25 23:08:20 +0800

    Add files via upload

---
+++ Text.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"Stack.h"
+
+void TestStack1()
+{
+	ST st;
+	StackInit(&st);
+
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+
+	printf("%d", StackTop(&st));
+
+	StackDestory(&st);
+}
+void TestStack2()
+{
+	ST st;
+	StackInit(&st);
+
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	printf("%d ", StackTop(&st));	//先出4
+	StackPop(&st);
+	printf("%d ", StackTop(&st));	//再出3
+	StackPop(&st);
+
+	StackPush(&st, 5);
+	StackPush(&st, 6);
+	StackPush(&st, 7);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+
+	StackDestory(&st);
+}
+int main()
+{
+	//TestStack1();
+	TestStack2();
+	return 0;
+}

eafee67b0078ca9372bbfcbe6b3c10338c8912c2
Thecodingshy <127670043+Thecodingshy@users.noreply.github.com>
2023-05-19 22:37:00 +0800

    Add files via upload

---
+++ 喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+////喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）。
+//#include<stdio.h>
+//int main()
+//{
+//	int Money = 0;
+//	int Cola = 0;
+//	int EmptyBottle = 0;
+//	printf("请投币：>");
+//	scanf("%d", &Money);	
+//	Cola = Money/1;
+//	EmptyBottle = Cola;
+//	while (EmptyBottle)
+//	{
+//		EmptyBottle = EmptyBottle / 2;
+//		Cola = Cola + EmptyBottle;
+//	}
+//	printf("%d ", Cola);
+//	return 0;
+//}
